4daf677dcca5ddd7a7ed6860bcdf3c9391058416
matthax <matt.bark11@gmail.com>
2016-11-05 15:01:57 -0400

    Added includes
    Added the required include items (header and footer)

---
+++ include/north_foot.php
@@ -0,0 +1,5 @@
+<div class="footer"></div>
+<script src="js/classList.js" type="text/javascript"></script>
+<script src="js/sizzle.js" type="text/javascript"></script>
+<script src="js/dom.js" type="text/javascript"></script>
+<script src="js/cart.js" type="text/javascript"></script>
---
+++ include/north_head.php
@@ -0,0 +1,7 @@
+<head>
+    <meta charset="utf-8">
+    <meta content='northwind,database,shop,food,goods,trader' name='keywords'>
+    <meta content="Northwind Trader's Website" name='description'>
+    <meta content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1' name='viewport'>
+    <title>Northwind Traders</title>
+</head>
