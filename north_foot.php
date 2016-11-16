c56784c601ca8239c0abb5b0bd58f9b6747a125b
Matt Bark <matthax@users.noreply.github.com>
2016-11-15 19:47:19 -0500

    Added include files
    Added include files outside of the include directory because lord knows Marius won't get it working with an include directory.

---
+++ north_foot.php
@@ -0,0 +1,7 @@
+<div class="footer"></div>
+<script src="js/classList.js" type="text/javascript"></script>
+<script src="js/sizzle.js" type="text/javascript"></script>
+<script src="js/dom.js" type="text/javascript"></script>
+<script src="js/cart.js" type="text/javascript"></script>
+<script src="js/northwind.js" type="text/javascript"></script>
+<script src="js/items.js" type="text/javascript"></script>
---
+++ north_head.php
@@ -0,0 +1,8 @@
+<head>
+    <meta charset="utf-8">
+    <meta content='northwind,database,shop,food,goods,trader' name='keywords'>
+    <meta content="Northwind Trader's Website" name='description'>
+    <meta content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1' name='viewport'>
+    <title>Northwind Traders</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
