1cbab7ecfb84d6789964bd480b2124c8c335f6a0
matthax <matt.bark11@gmail.com>
2016-11-29 15:16:22 -0500

    Added required src files

--- north_foot.php
+++ north_foot.php
@@ -2,6 +2,7 @@
 <script src="js/classList.js" type="text/javascript"></script>
 <script src="js/sizzle.js" type="text/javascript"></script>
 <script src="js/dom.js" type="text/javascript"></script>
+<script src="js/toast.js" type="text/javascript"></script>
 <script src="js/cart.js" type="text/javascript"></script>
 <script src="js/northwind.js" type="text/javascript"></script>
 <script src="js/items.js" type="text/javascript"></script>
