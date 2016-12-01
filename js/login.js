be9d221889c2e315b4b7e8882f67267b53d1b90b
kmorris95 <kmorris95@comcast.net>
2016-11-30 22:02:19 -0500

    shortened inputs

--- js/login.js
+++ js/login.js
@@ -16,9 +16,9 @@ window.pages.login = function () {
     login.load = function (hash, callback) {
         var form = dom.create("form", {action: dom.url + "/api/login", method: "POST" })
             /*.append(dom.create("h1", {text: "Login"}))*/
-            .append(dom.create("input", {type: "text", required: "required", placeholder: "User ID", name: "user_id", "class": "material-input"}))
-            .append(dom.create("input", {type: "password", required: "required", placeholder: "Password", name: "password", "class": "material-input"}))
-            .append(dom.create("input", {type: "submit", placeholder: "", name: "", text: "Register", "class": "material-button"}));
+            .append(dom.create("input", {type: "text", required: "required", placeholder: "User ID", name: "user_id", "class": "material-input-small"}))
+            .append(dom.create("input", {type: "password", required: "required", placeholder: "Password", name: "password", "class": "material-input-small"}))
+            .append(dom.create("input", {type: "submit", placeholder: "", name: "", text: "Register", "class": "material-button-small"}));
         callback(form);
     };
     northwind.registerPageHandler("Login", /^\/login\/?/i, login.load);
