c3f64fb22c095740a60e14b3d2ad7dee5d1b2e77
kmorris95 <kmorris95@comcast.net>
2016-11-30 23:54:25 -0500

    removed required attr for some fields

--- js/register.js
+++ js/register.js
@@ -15,15 +15,15 @@ window.pages.register = function () {
             /*.append(dom.create("h1", {text: "Register"}))*/
             .append(dom.create("input", {type: "text", required: "required", placeholder: "First Name", name: "first_name", "class": "material-input-small"}))
             .append(dom.create("input", {type: "text", required: "required", placeholder: "Last Name", name: "last_name", "class": "material-input-small"}))
-            .append(dom.create("input", {type: "text", required: "required", placeholder: "Company", name: "company", "class": "material-input-small"}))
+            .append(dom.create("input", {type: "text", placeholder: "Company", name: "company", "class": "material-input-small"}))
             .append(dom.create("input", {type: "text", required: "required", placeholder: "Email Address", name: "email_address", "class": "material-input-small"}))
-            .append(dom.create("input", {type: "text", required: "required", placeholder: "Job Title", name: "job_title", "class": "material-input-small"}))
-            .append(dom.create("input", {type: "text", required: "required", placeholder: "Address", name: "address", "class": "material-input-small"}))
-            .append(dom.create("input", {type: "text", required: "required", placeholder: "City", name: "city", "class": "material-input-small"}))
-            .append(dom.create("input", {type: "text", required: "required", placeholder: "State", name: "state_province", "class": "material-input-small"}))
-            .append(dom.create("input", {type: "text", required: "required", placeholder: "Zip Code", name: "zip_postal_code", "class": "material-input-small"}))
-            .append(dom.create("input", {type: "text", required: "required", placeholder: "Country", name: "country_region", "class": "material-input-small"}))
-            .append(dom.create("input", {type: "text", required: "required", placeholder: "Home Phone", name: "home_phone", "class": "material-input-small"}))
+            .append(dom.create("input", {type: "text", placeholder: "Job Title", name: "job_title", "class": "material-input-small"}))
+            .append(dom.create("input", {type: "text", placeholder: "Address", name: "address", "class": "material-input-small"}))
+            .append(dom.create("input", {type: "text", placeholder: "City", name: "city", "class": "material-input-small"}))
+            .append(dom.create("input", {type: "text", placeholder: "State", name: "state_province", "class": "material-input-small"}))
+            .append(dom.create("input", {type: "text", placeholder: "Zip Code", name: "zip_postal_code", "class": "material-input-small"}))
+            .append(dom.create("input", {type: "text", placeholder: "Country", name: "country_region", "class": "material-input-small"}))
+            .append(dom.create("input", {type: "text", placeholder: "Home Phone", name: "home_phone", "class": "material-input-small"}))
             .append(dom.create("input", {type: "submit", placeholder: "", name: "", text: "Register", "class": "material-button-small"}))
             .append(dom.a("#/login", {text: "Already Registered? Login In"}));
         callback(form);
