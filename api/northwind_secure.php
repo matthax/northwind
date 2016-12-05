4520b0bbc4102c2ff6a96e48fe1710761255c3a2
kmorris95 <Rocket@v1129wireless60.sju.edu>
2016-12-05 18:30:14 -0500

    added secure

---
+++ api/northwind_secure.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+    
+function isLoggedIn() {
+    $auth = '09F911029D74E35BD84156C5635688C0';
+    if (isset($_SESSION['auth']) && strcmp($_SESSION['auth'], $auth) === 0) {
+        return True;
+    }
+    return False;
+}
+if (!isLoggedIn()) {
+    $result = array("error" => true, "message" => "You must be logged in to access this page", "reason" => "auth", "session" => $_SESSION);
+    die(json_encode($result));
+}
+?>
